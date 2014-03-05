ad7e2c51bc5e4334831da35e98c514935af1c01f
wq1 <wq1@users.noreply.github.com>
2014-03-05 19:10:35 +0900

    zerogen

---
+++ zerogen.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+int main(void)
+{
+	fflush(stdout);
+	_setmode(_fileno(stdout), _O_BINARY);
+
+	int i = 0;
+	int j;
+
+	while (1) {
+	i = 0;
+	j = fwrite(&i,1,1,stdout);
+	if (j != 1)
+		break; // stdoutの受取先がなくなったら抜ける
+	}
+	
+	return 0;
+}
