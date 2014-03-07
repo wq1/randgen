31ee2212026f882cb93d823c2e2d75ed81b9aac3
wq1 <wq1@users.noreply.github.com>
2014-03-07 18:39:57 +0900

    add: display output-size when quit

--- randgen.c
+++ randgen.c
@@ -1,11 +1,10 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <fcntl.h>
 #include <time.h>
 #include <unistd.h>
 
-int main (int argc, char *argv[]) {
+int main( int argc, char *argv[] ) {
 
 	int i = 0;
 	int j = 0;
@@ -27,26 +26,32 @@ int main (int argc, char *argv[]) {
 			char num[32];
 			sprintf(num, "%s", optarg);
 
-			char si[2];
-			sprintf(si, "%c", num[strlen(num)-1]);
+			int si = num[strlen(num)-1];
+			num[strlen(num)-1] = '\0';
 
-			num[strlen(num)-1] = '\0'; // 単位消去
-
-			     if ( strcmp(si, "K") == 0 )
+			switch (si) {
+			case 'K':
 				tgt = atoll(num)*1024;
-			else if ( strcmp(si, "M") == 0 )
+				break;
+			case 'M':
 				tgt = atoll(num)*1024*1024;
-			else if ( strcmp(si, "G") == 0 )
+				break;
+			case 'G':
 				tgt = atoll(num)*1024*1024*1024;
-			else if ( strcmp(si, "T") == 0 )
+				break;
+			case 'T':
 				tgt = atoll(num)*1024*1024*1024*1024;
-			else if ( strcmp(si, "P") == 0 )
+				break;
+			case 'P':
 				tgt = atoll(num)*1024*1024*1024*1024*1024;
-			else if ( strcmp(si, "E") == 0 )
+				break;
+			case 'E':
 				tgt = atoll(num)*1024*1024*1024*1024*1024*1024;
-			else {
+				break;
+			default:
 				sprintf(num, "%s", optarg);
 				tgt = atoll(num);
+				break;
 			}
 
 			break;
@@ -64,15 +69,16 @@ int main (int argc, char *argv[]) {
 	srand((unsigned) time(NULL));
 
 	while (1) {
-		if ( z != 1 ) { // 遅くならないか心配
+		if (z != 1) { // 遅くならないか心配
 			//i = rand()%0x100; // 0x00-0xFF 低精度
-			i = m + (int)( (double) rand() * (double)(M-m+1) / (double)(RAND_MAX+1) ); // 高精度
+			i = m + (int)((double) rand() * (double)(M-m+1) / (double)(RAND_MAX+1)); // 高精度
 		}
 		j = fwrite(&i,1,1,stdout);
-		out = out+1;
-		if ( out == tgt || j != 1 ) // 目標サイズに達するか受取先が終了する
+		out++; // out = out+1;
+		if (out == tgt || j != 1) // 目標サイズに達するか受取先が終了する
 			break;
 	}
 
+	fprintf(stderr, "%d byte(s) out\n", out);
 	return 0;
 }
