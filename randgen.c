ec4897670f9173b702aedcabc2b81b1b4aa5842b
wq1 <wq1@users.noreply.github.com>
2014-03-07 20:18:29 +0900

    add: display elapsed time when quit

--- randgen.c
+++ randgen.c
@@ -14,6 +14,7 @@ int main(int argc, char *argv[]) {
 	long long tgt = 0;
 	long long out = 0;
 	int c;
+	time_t t0,t1;
 
 	while ((c = getopt(argc, argv, "zs:")) != -1) {
 		switch (c) {
@@ -68,8 +69,10 @@ int main(int argc, char *argv[]) {
 
 	srand((unsigned) time(NULL));
 
+	t0 = time(NULL);
+
 	while (1) {
-		if (z != 1) { // 遅くならないか心配
+		if (z != 1) {
 			//i = rand()%0x100; // 0x00-0xFF 低精度
 			i = m + (int)((double) rand() * (double)(M-m+1) / (double)(RAND_MAX+1)); // 高精度
 		}
@@ -79,6 +82,10 @@ int main(int argc, char *argv[]) {
 			break;
 	}
 
+	t1 = time(NULL);
+
 	fprintf(stderr, "%d byte(s) out\n", out);
+	fprintf(stderr, "%d second(s) elapsed\n", (long)(t1 - t0));
+
 	return 0;
 }
